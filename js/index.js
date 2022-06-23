07d5b1f1e6145f785e2fc680dc7743c0c45bc12d
Zeynab Yad <zeynab.design@gmail.com>
2022-06-23 15:55:53 +0430

    Add create cat function

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
 const products = [
-  "product 1",
-  "product 2",
-  "product 3",
-  "product 4",
-  "product 5",
+    "product 1",
+    "product 2",
+    "product 3",
+    "product 4",
+    "product 5",
 ];
 
 //create shopping cart table from products array
-let cardContent = "";
-for (const product in products) {
-  cardContent += `<tr>
+function createCart() {
+    let cardContent = "";
+    for (const product in products) {
+        cardContent += `<tr>
     <td>${products[product]}</td>
     <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${product})"></i></td>
   </tr>`;
 
+    }
+    document.querySelector('table').innerHTML = cardContent;
 }
-document.querySelector('table').innerHTML=cardContent;
-console.log(cardContent);
