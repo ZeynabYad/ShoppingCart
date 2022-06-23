8a65d753c1413cba744d047e2f8997ea51c7dba7
Zeynab Yad <zeynab.design@gmail.com>
2022-06-23 17:23:18 +0430

    Add sort function.

--- js/index.js
+++ js/index.js
@@ -10,19 +10,37 @@ const products = [
 
 function createCartTable() {
     let cardContent = "";
-    products.forEach(function (productName,productIndex) {
+    products.forEach(function (productName, productIndex) {
         cardContent += `<tr>
         <td>${productName}</td>
         <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${productIndex})"></i></td>
       </tr>`
     });
-    document.querySelector('table').innerHTML = cardContent;
-
+    document.querySelector('tbody').innerHTML = cardContent;
 }
 
+//add event listeners to sort icons
+document.querySelector('.fa-arrow-down-a-z').addEventListener('click', function () {
+    sortProductName(true);
+})
+document.querySelector('.fa-arrow-down-z-a').addEventListener('click', function () {
+    sortProductName(false);
+})
 
 //delete product from shopping cart
 function deleteProduct(productIndex) {
     products.splice(productIndex, 1);
     createCartTable();
+}
+
+//sort productName function
+function sortProductName(direction) {
+    if (direction) {
+        products.sort()
+        createCartTable();
+    }
+    else {
+        products.sort().reverse()
+        createCartTable();
+    };
 }
