ae6e80a280209d5b035a5cb77506d29368b5bf73
Zeynab Yad <zeynab.design@gmail.com>
2022-06-23 16:56:02 +0430

    Update createCartTable with foreach.

--- js/index.js
+++ js/index.js
@@ -7,16 +7,19 @@ const products = [
 ];
 
 //create shopping cart table from products array
+
 function createCartTable() {
     let cardContent = "";
-    for (const product in products) {
+    products.forEach(function (productName,productIndex) {
         cardContent += `<tr>
-    <td>${products[product]}</td>
-    <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${product})"></i></td>
-  </tr>`;
-    }
+        <td>${productName}</td>
+        <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${productIndex})"></i></td>
+      </tr>`
+    });
     document.querySelector('table').innerHTML = cardContent;
+
 }
+
 
 //delete product from shopping cart
 function deleteProduct(productIndex) {
