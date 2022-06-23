eeca609ce939e23f39e34887264c5d62c26fd90b
Zeynab Yad <zeynab.design@gmail.com>
2022-06-23 15:45:43 +0430

    Create table from products array

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const products = [
+  "product 1",
+  "product 2",
+  "product 3",
+  "product 4",
+  "product 5",
+];
+
+//create shopping cart table from products array
+let cardContent = "";
+for (const product in products) {
+  cardContent += `<tr>
+    <td>${products[product]}</td>
+    <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${product})"></i></td>
+  </tr>`;
+
+}
+document.querySelector('table').innerHTML=cardContent;
+console.log(cardContent);
