38a54d4dd2042ac4aa41efb2348340de84921f5b
Zeynab Yad <zeynab.design@gmail.com>
2022-07-10 23:00:45 +0430

    Refactor function createCartTable. use createCartTable for searchResult.

--- js/index.js
+++ js/index.js
@@ -1,38 +1,78 @@
 const products = [
-    { productName: 'Tropicana Apple Juice', productPrice: 35, productQuantity: 1, productImageSrc: 'TropicanaAppleJuice.jpg' },
-    { productName: 'McCafe Medium Coffee', productPrice: 45, productQuantity: 5, productImageSrc: 'McCafeMediumCoffee.jpg' },
-    { productName: 'KitKat Wafer', productPrice: 20, productQuantity: 3, productImageSrc: 'KitKatWafer.jpg' },
-    { productName: 'Lays Potato Chips', productPrice: 25, productQuantity: 2, productImageSrc: 'LaysPotatoChips.jpg' },
-    { productName: 'Cheetos Puffs Cheese', productPrice: 20, productQuantity: 1, productImageSrc: 'CheetosPuffsCheese.jpg' },
+    {
+        productName: "Tropicana Apple Juice",
+        productPrice: 35,
+        productQuantity: 1,
+        productImageSrc: "TropicanaAppleJuice.jpg",
+    },
+    {
+        productName: "McCafe Medium Coffee",
+        productPrice: 45,
+        productQuantity: 5,
+        productImageSrc: "McCafeMediumCoffee.jpg",
+    },
+    {
+        productName: "KitKat Wafer",
+        productPrice: 20,
+        productQuantity: 3,
+        productImageSrc: "KitKatWafer.jpg",
+    },
+    {
+        productName: "Lays Potato Chips",
+        productPrice: 25,
+        productQuantity: 2,
+        productImageSrc: "LaysPotatoChips.jpg",
+    },
+    {
+        productName: "Cheetos Puffs Cheese",
+        productPrice: 20,
+        productQuantity: 1,
+        productImageSrc: "CheetosPuffsCheese.jpg",
+    },
 ];
-
-function createCartTable() {
-    let rows = products.map((product, productIndex) =>
-        `<tr class='align-middle'>
-        <th>${productIndex + 1}</th>
-        <th><img width="80" height="80" src="images/${product.productImageSrc}"></th>
-        <td class='text-start'>${product.productName}</td>
-        <td>$${product.productPrice}</td>
-        <td class="fw-bold"><i class="fa-solid fa-minus btn btn-warning btn-sm me-3" onClick="reduceQuantity(${productIndex})"></i>
-        ${product.productQuantity}
-        <i class="fa-solid fa-plus btn btn-success btn-sm ms-3" onClick="addQuantity(${productIndex})"></i></td>
-        <td class="fw-bold">$${Number(product.productPrice) * Number(product.productQuantity)}</td>
-        <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${productIndex})"></i></td>
-      </tr>`
+//function createCartTable
+function createCartTable(array) {
+    let rows = array.map(
+        (item, itemIndex) =>
+            `<tr class='align-middle'>
+          <th>${itemIndex + 1}</th>
+          <th><img width="80" height="80" src="images/${item.productImageSrc
+            }"></th>
+          <td class='text-start'>${item.productName}</td>
+          <td>$${item.productPrice}</td>
+          <td class="fw-bold"><i class="fa-solid fa-minus btn btn-warning btn-sm me-3" onClick="reduceQuantity(${itemIndex})"></i>
+          ${item.productQuantity}
+          <i class="fa-solid fa-plus btn btn-success btn-sm ms-3" onClick="addQuantity(${itemIndex})"></i></td>
+          <td class="fw-bold">$${Number(item.productPrice) * Number(item.productQuantity)
+            }</td>
+          <td><i class="fa-solid fa-trash-can text-danger" onClick="deleteProduct(${itemIndex})"></i></td>
+        </tr>`
     );
+    document.querySelector("tbody").innerHTML = rows.join("");
+    calculateOrderTotal(array);
+}
 
-    document.querySelector('tbody').innerHTML = rows.join('');
-    calculateOrderTotal();
+// Add Eventlistener to SearchBtn
+document.querySelector("#searchBtn").addEventListener("click", doSearch);
+document.querySelector("#keyword").addEventListener("keyup", doSearch);
+
+function doSearch() {
+    let keyword = document.querySelector("#keyword").value.toLowerCase();
+    const searchResult = products.filter((item) => item.productName.toLowerCase().includes(keyword))
+    createCartTable(searchResult)
 }
 
 //add event listeners to sort icons
-document.querySelector('.fa-arrow-down-a-z').addEventListener('click', function () {
-    sortProductName(true);
-})
-document.querySelector('.fa-arrow-up-z-a').addEventListener('click', function () {
-    sortProductName(false);
-})
-
+document
+    .querySelector(".fa-arrow-down-a-z")
+    .addEventListener("click", function () {
+        sortProductName(true);
+    });
+document
+    .querySelector(".fa-arrow-up-z-a")
+    .addEventListener("click", function () {
+        sortProductName(false);
+    });
 
 // sort productName function
 function sortProductName(direction) {
@@ -43,19 +83,18 @@ function sortProductName(direction) {
             if (fa < fb) return -1;
             if (fa > fb) return 1;
             return 0;
-        })
-        createCartTable();
-    }
-    else {
+        });
+        createCartTable(products);
+    } else {
         products.sort(function (a, b) {
             let fa = a.productName.toLowerCase();
             let fb = b.productName.toLowerCase();
             if (fa > fb) return -1;
             if (fa < fb) return 1;
             return 0;
-        })
-        createCartTable();
-    };
+        });
+        createCartTable(products);
+    }
 }
 
 // delete product from shopping cart
@@ -64,32 +103,34 @@ function deleteProduct(productIndex) {
     createCartTable();
 }
 
-//Add product Quantity 
+//Add product Quantity
 function addQuantity(productIndex) {
     products[productIndex].productQuantity++;
     createCartTable();
 }
-//Reduce product Quantity 
+//Reduce product Quantity
 function reduceQuantity(productIndex) {
-
     if (products[productIndex].productQuantity > 1) {
         products[productIndex].productQuantity--;
         createCartTable();
-    };
+    }
 }
 
 //function calculateOrderTotal to calculate sum order
-function calculateOrderTotal() {
-    let sum = products.reduce((accumulator, currentProduct) => accumulator + (currentProduct.productPrice * currentProduct.productQuantity), 0);
-    console.log(sum)
-    document.querySelector('tfoot').innerHTML = 
-    `<tr>
-    <td colspan=""></td>
-    <td colspan=""></td>
-    <td colspan=""></td>
-    <td colspan=""></td>
-    <td colspan=""></td>
-    <td class="fw-bold" colspan="">TOTAL: $${sum}</td>
-    <td colspan=""></td>
-    <tr>`;
+function calculateOrderTotal(array) {
+    let sum = array.reduce(
+        (accumulator, currentProduct) =>
+            accumulator +
+            currentProduct.productPrice * currentProduct.productQuantity,
+        0
+    );
+    document.querySelector("tfoot").innerHTML = `<tr>
+<td colspan=""></td>
+<td colspan="3"><button class='btn btn-primary'>Continue Check out</button></td>
+<td colspan=""></td>
+<td class="fw-bold" colspan="">TOTAL: $${sum}</td>
+<td colspan=""></td>
+<tr>`;
 }
+
+
